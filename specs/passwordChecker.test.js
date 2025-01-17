3ffc107289f7bec485b24850ab18b6aa10152cd3
amelie-kienge-vinci <amelie.kienge@student.vinci.be>
2025-01-17 17:14:24 +0100

    add:should return true when password contains special char

--- specs/passwordChecker.test.js
+++ specs/passwordChecker.test.js
@@ -6,13 +6,15 @@ describe("passwordChecker", () => {
   it("should return false when password is less than 8 characters", () => {
     expect(main.passwordChecker("1234567")).toBe(false);
   });
- 
+
   it("should return true when password is 8 characters or more", () => {
     expect(main.passwordChecker("12345678")).toBe(true);
   });
   it("should return false when password don't contains any special char", () => {
-    expect(main.passwordChecker("123!djddddddd")).toBe(false);
-    });
-
-
+    expect(main.passwordChecker("123djddddddd")).toBe(false);
+  });
+  it("should return true when password contains special char", () => {
+    expect(main.passwordChecker("123djddddd!dd")).toBe(true);
+  });
+  
 });
