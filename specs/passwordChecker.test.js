8db6b17e1a8e52a542a831f06d243d4eb4425ef1
amelie-kienge-vinci <amelie.kienge@student.vinci.be>
2025-01-17 17:00:10 +0100

    add: when given less than 8 char return false

--- specs/passwordChecker.test.js
+++ specs/passwordChecker.test.js
@@ -2,13 +2,11 @@ import { Main } from "../src/passwordChecker";
 
 describe("passwordChecker", () => {
   let main = new Main();
-  
+
   it("should return false when password is less than 8 characters", () => {
     expect(main.passwordChecker("1234567")).toBe(false);
   });
-  it("should return true when password is 8 characters or more", () => {
-    expect(main.passwordChecker("12345678")).toBe(true);
-  });
+ 
 
 
 
